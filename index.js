81a5eaec5cd5e5b56fb298290b6e87666ec0e404
Alexander Collins <ac@Alexanders-MacBook-Pro.local>
2018-12-19 19:09:30 +1100

    Adding network fetch

--- index.js
+++ index.js
@@ -91,9 +91,12 @@ let validate_connection_and_scan = () => {
                         display.write_text(`Found preconfigured network\nUpdating network config.`);
                         let base_network_config = "ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev\nupdate_config=1\nnetwork={\n\tssid='safedome0123'\n\tpsk='Collins02'\n\tkey_mgmt=WPA-PSK\n}\n\n";
                         let custom_network_config = `network={\n\tssid='${body.detail[0].username}'\n\tpsk='${body.detail[0].password}'\n\tkey_mgmt=WPA-PSK}\n`
+                        console.log("calling network update script");
                         let network_update_script = exec(
                             `sudo echo ${base_network_config + custom_network_config} > /etc/wpa_supplicant/wpa_supplicant.conf`, 
                             function(_, stdout, stderr) {
+                                console.log(stdout);
+                                console.log(stderr);
                                 setTimeout(
                                     validate_connection_and_scan,
                                     SCAN_INTERVAL
