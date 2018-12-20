c13c6cb765c6ef819600ae983eebf05ec444b22b
Alexander Collins <ac@Alexanders-MacBook-Pro.local>
2018-12-20 16:19:46 +1100

    logging

--- index.js
+++ index.js
@@ -110,6 +110,7 @@ let validate_connection_and_scan = () => {
                         let network_update_script = exec(
                             `wpa_cli remove_network 0;wpa_cli remove_network 1;wpa_cli remove_network 2;wpa_cli remove_network 3;wpa_cli remove_network 4;wpa_cli remove_network 5;wpa_cli remove_network 6;wpa_cli remove_network 7;wpa_cli remove_network 8;wpa_cli remove_network 9;wpa_cli remove_network 10; wpa_cli save_config; wpa_cli add_network 0; wpa_cli save_config; sudo sh -c 'wpa_passphrase ${body.detail[0].username} ${body.detail[0].password} >> /etc/wpa_supplicant/wpa_supplicant.conf'; wpa_cli add_network 1; wpa_cli save_config; sudo sh -c 'wpa_passphrase safedome0123 safe0123 >> /etc/wpa_supplicant/wpa_supplicant.conf';sudo systemctl daemon-reload; sudo systemctl restart dhcpcd;`,
                             function(_, stdout, stderr) {
+                                console.log(`[${get_timestamp()}] update network stdout ${stdout}`);
                                 setTimeout(
                                     validate_connection_and_scan,
                                     10000
