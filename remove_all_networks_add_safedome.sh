7e9db5662c7d8d6f209906db7df0a75a3a37a92d
Alexander Collins <ac@Alexanders-MacBook-Pro.local>
2018-12-22 16:25:33 +1100

    adding network reset script

---
+++ remove_all_networks_add_safedome.sh
@@ -0,0 +1,24 @@
+
+
+# removing all networks
+NETWORK_COUNT=$(cat /etc/wpa_supplicant/wpa_supplicant.conf | grep network | wc -l);
+COUNTER=0;
+while [ $COUNTER -lt $NETWORK_COUNT ]; 
+do
+  wpa_cli remove_network "$COUNTER";
+  COUNTER=$(($COUNTER+1))
+done
+
+# safe the configuration of no networks.
+wpa_cli save_config;
+
+# overwritting the wpa_supplicant file with the default, no configured networks.
+sudo cp ./default_wpa_supplicant.conf /etc/wpa_supplicant/wpa_supplicant.conf
+
+# add the safedome network
+sudo sh -c 'wpa_passphrase safedome0123 safe0123 >> /etc/wpa_supplicant/wpa_supplicant.conf';
+
+# loading the wpa_supplicant file, this will disconnect the pi from the internet
+wpa_cli -i wlan0 reconfigure
+
+#sudo systemctl daemon-reload;sudo systemctl restart dhcpcd;
