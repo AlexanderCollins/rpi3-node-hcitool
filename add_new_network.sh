c3a94f6055ff948ee88ea397691023f5b637cb4c
Alexander Collins <ac@Alexanders-MacBook-Pro.local>
2018-12-22 16:56:37 +1100

    update

--- add_new_network.sh
+++ add_new_network.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 # add the new network to wpa_supplicant.
 COMMAND="wpa_passphrase '$1' '$2' >> /etc/wpa_supplicant/wpa_supplicant.conf";
-sudo sh -c $COMMAND;
+echo $COMMAND;
+sudo sh -c "$COMMAND";
