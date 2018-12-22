3b6e621ac6cc3c70f81fae636d17d3feffabe896
Alexander Collins <ac@Alexanders-MacBook-Pro.local>
2018-12-22 17:27:41 +1100

    adding set network block priorities script

---
+++ set_network_block_priorities.js
@@ -0,0 +1,51 @@
+var fs = require('fs');
+const { exec } = require('child_process');
+
+let get_timestamp = () => {
+    /* Format current datetime to human readable timestamp. */
+    return (new Date()).toJSON().slice(0, 19).replace(/[-T]/g, ':');
+};
+
+
+function update_supplicant(network_block_count) {
+    var array = fs.readFileSync('/etc/wpa_supplicant/wpa_supplicant.conf').toString().split("\n");
+    let priority_count = 0;
+    let new_array = new Array();
+
+    /* Iterate over each line in the existing wpa_supplicant file and add priority to each network block */
+    for(var index = 0; index < array.length; ++index) {
+        if(array[index].indexOf('}') !== -1){
+            new_array.push(`\tpriority=${network_block_count - priority_count}`);
+            priority_count++;
+        }
+        new_array.push(array[index]);
+    }
+
+    // write out the new supplicant file to a temp file.
+    let write_out_to_temp = exec(`sudo touch temp.conf && sudo echo '${new_array.join("\n")}' > ./temp.conf`, function(_, stdout, __){
+        console.log(stdout);
+        let copy_temp_to_supplicant = exec("", function(_, stdout, __){
+            console.log(stdout);
+        });
+
+        copy_temp_to_supplicant.on('sudo cp ./temp.conf /etc/wpa_supplicant/wpa_supplicant.conf', function (code) {
+            console.log(`[${get_timestamp()}] <copy_temp_to_supplicant> command line function exited with code: <${code}> (0: success, 1: failure).`);
+        });
+    });
+
+    write_out_to_temp.on('exit', function (code) {
+        console.log(`[${get_timestamp()}] <write_out_to_temp> command line function exited with code: <${code}> (0: success, 1: failure).`);
+    });
+}
+
+
+
+let network_count_command = exec("cat /etc/wpa_supplicant/wpa_supplicant.conf | grep network | wc -l", function(_, stdout, __){
+    let network_block_count = parseInt(stdout);
+    update_supplicant(network_block_count);
+});
+
+/* Log the result of the network count command */
+network_count_command.on('exit', function (code) {
+    console.log(`[${get_timestamp()}] <network_count_command> command line function exited with code: <${code}> (0: success, 1: failure).`);
+});
